7b574bad25caad718d7260dd3e8239b6d2460ef2
KDercksen <mail@koendercksen.com>
2023-11-30 11:46:06 +0100

    Add code for estimating cost and generating data

---
+++ README.md
@@ -0,0 +1,19 @@
+In the examples for this workshop, we will use the OpenAI and Together APIs.
+For both of these, you need an API key. You can get an OpenAI API key
+[here](https://platform.openai.com/). You can get a Together API key
+[here](https://api.together.xyz/).
+
+Create a `.env` file in the root directory of this project and add the
+following:
+
+```bash
+# .env
+OPENAI_API_KEY=<your key here>
+TOGETHER_API_KEY=<your key here>
+```
+
+Install the required Python dependencies (inside a virtual environment):
+
+```bash
+pip install -r requirements.txt
+```
---
+++ estimate_cost.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+import tiktoken
+
+encoder = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+with open("data.json") as f:
+    data = json.load(f)
+
+prompt_price = 0.001  # per 1000 tokens
+generated_price = 0.002  # per 1000 tokens
+
+total_price = 0
+for sample in data:
+    prompt_tokens = encoder.encode(sample["prompt"])
+    generated_tokens = encoder.encode(sample["completion"])
+    prompt_cost = len(prompt_tokens) / 1000 * prompt_price
+    generated_cost = len(generated_tokens) / 1000 * generated_price
+    total_price += prompt_cost + generated_cost
+
+print(f"Total cost for this data file: ${total_price:.2f}")
